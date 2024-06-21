784afb044d9bdcc480bc8044a9fc0a9b67196397
AnEvenStrangerCamber <jay.idema@gmail.com>
2024-06-21 17:49:27 -0400

    Namespace adjustment: put componentList and system function signature extractors in ECS

--- ECS_src/ESC_Manager_SystemTraits.h
+++ ECS_src/ESC_Manager_SystemTraits.h
@@ -5,17 +5,15 @@
 #include <tuple>
 #include <functional>
 
+namespace ECS {
+    template<typename... Cs>
+    struct ComponentList {
+    };
 
-template <typename... Cs>
-struct ComponentList {};
 
-//template <typename R, typename Res, typename... Args>
-//struct sys_function_traits<Res, R(*)(std::tuple<Res*, Args...>&)> : public sys_function_traits<Res, R(*)(Res*, Args...)> {
-//	static constexpr bool UsesTuple = true;
-//};
-
-template <typename Res, size_t N, typename CompList, typename Fn>
-struct sys_function_traits {};
+    template<typename Res, size_t N, typename CompList, typename Fn>
+    struct sys_function_traits {
+    };
 
 /*********************************************************************************************
 * FUNCTION Resource Types
@@ -29,98 +27,82 @@ struct sys_function_traits {};
  * Systems can request the single Resource, distinct from the component system; in that case we take special care to provide it.
  */
 
-template <typename R, size_t N, typename CompList, typename Res, typename... Args>
-struct sys_function_traits<Res, N, CompList, R(*)(std::tuple<Res*, Args...>&)>
-{
-    using arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
-    using full_arg_types = FuncArgs<
-    std::decay_t<std::remove_pointer_t<std::remove_cv_t<Res>>>,
-    std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...
-    >;
-    static constexpr bool UsesTuple = true;
-    static constexpr bool UsesEntityID = false;
-    static constexpr bool UsesRes = true;
-};
+    template<typename R, size_t N, typename CompList, typename Res, typename... Args>
+    struct sys_function_traits<Res, N, CompList, R(*)(std::tuple<Res *, Args...> &)> {
+        using arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
+        using full_arg_types = FuncArgs<
+                std::decay_t<std::remove_pointer_t<std::remove_cv_t<Res>>>,
+                std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...
+        >;
+        static constexpr bool UsesTuple = true;
+        static constexpr bool UsesEntityID = false;
+        static constexpr bool UsesRes = true;
+    };
 
 // NONE TUPLE ARGS
-template <typename R, size_t N, typename CompList, typename Res, typename... Args>
-struct sys_function_traits<Res, N, CompList, R(*)(Res*, Args...)>
-{
-    using arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
-    using full_arg_types = FuncArgs<
-    std::decay_t<std::remove_pointer_t<std::remove_cv_t<Res>>>,
-    std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...
-    >;
-    static constexpr bool UsesRes = true;
-    static constexpr bool UsesEntityID = false;
-    static constexpr bool UsesTuple = false;
-};
+    template<typename R, size_t N, typename CompList, typename Res, typename... Args>
+    struct sys_function_traits<Res, N, CompList, R(*)(Res *, Args...)> {
+        using arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
+        using full_arg_types = FuncArgs<
+                std::decay_t<std::remove_pointer_t<std::remove_cv_t<Res>>>,
+                std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...
+        >;
+        static constexpr bool UsesRes = true;
+        static constexpr bool UsesEntityID = false;
+        static constexpr bool UsesTuple = false;
+    };
 
-template <typename R, size_t N, typename... Cs, typename Res, typename... Args>
-struct sys_function_traits<Res, N, ComponentList<Cs...>, R(*)(std::tuple<Res*, size_t*, CoreComponentManager<Cs...>*, Args...>&)>
-{
-using arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
-using full_arg_types = FuncArgs<
-std::decay_t<std::remove_pointer_t<std::remove_cv_t<Res>>>,
-std::decay_t<std::remove_pointer_t<std::remove_cv_t<size_t>>>,
-std::decay_t<std::remove_pointer_t<std::remove_cv_t<CoreComponentManager<Cs...>>>>,
-std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...
->;
-static constexpr bool UsesTuple = true;
-static constexpr bool UsesEntityID = true;
-static constexpr bool UsesRes = true;
-};
+    template<typename R, size_t N, typename... Cs, typename Res, typename... Args>
+    struct sys_function_traits<Res, N, ComponentList<Cs...>, R(*)(
+            std::tuple<Res *, size_t *, CoreComponentManager<Cs...> *, Args...> &)> {
+        using arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
+        using full_arg_types = FuncArgs<
+                std::decay_t<std::remove_pointer_t<std::remove_cv_t<Res>>>,
+                std::decay_t<std::remove_pointer_t<std::remove_cv_t<size_t>>>,
+                std::decay_t<std::remove_pointer_t<std::remove_cv_t<CoreComponentManager<Cs...>>>>,
+                std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...
+        >;
+        static constexpr bool UsesTuple = true;
+        static constexpr bool UsesEntityID = true;
+        static constexpr bool UsesRes = true;
+    };
 
 // NONE TUPLE ARGS
-template <typename R, size_t N, typename... Cs, typename Res, typename... Args>
-struct sys_function_traits<Res, N, ComponentList<Cs...>, R(*)(Res*, size_t*, CoreComponentManager<Cs...>*, Args...)>
-{
-using arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
-using full_arg_types = FuncArgs<
-std::decay_t<std::remove_pointer_t<std::remove_cv_t<Res>>>,
-std::decay_t<std::remove_pointer_t<std::remove_cv_t<size_t>>>,
-std::decay_t<std::remove_pointer_t<std::remove_cv_t<CoreComponentManager<Cs...>>>>,
-std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...
->;
-static constexpr bool UsesRes = true;
-static constexpr bool UsesTuple = false;
-static constexpr bool UsesEntityID = true;
-};
+    template<typename R, size_t N, typename... Cs, typename Res, typename... Args>
+    struct sys_function_traits<Res, N, ComponentList<Cs...>, R(*)(Res *, size_t *, CoreComponentManager<Cs...> *,
+                                                                  Args...)> {
+        using arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
+        using full_arg_types = FuncArgs<
+                std::decay_t<std::remove_pointer_t<std::remove_cv_t<Res>>>,
+                std::decay_t<std::remove_pointer_t<std::remove_cv_t<size_t>>>,
+                std::decay_t<std::remove_pointer_t<std::remove_cv_t<CoreComponentManager<Cs...>>>>,
+                std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...
+        >;
+        static constexpr bool UsesRes = true;
+        static constexpr bool UsesTuple = false;
+        static constexpr bool UsesEntityID = true;
+    };
 
 /*********************************************************************************************
 * NON-Resource Types
 **********************************************************************************************/
 
-template <typename R, size_t N, typename CompList, typename Res, typename... Args>
-struct sys_function_traits<Res, N, CompList, R(*)(std::tuple <Args...>&)>
-{
-    using arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
-    using full_arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
-    static constexpr bool UsesRes = false;
-    static constexpr bool UsesEntityID = false;
-    static constexpr bool UsesTuple = true;
-};
+    template<typename R, size_t N, typename CompList, typename Res, typename... Args>
+    struct sys_function_traits<Res, N, CompList, R(*)(std::tuple<Args...> &)> {
+        using arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
+        using full_arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
+        static constexpr bool UsesRes = false;
+        static constexpr bool UsesEntityID = false;
+        static constexpr bool UsesTuple = true;
+    };
 
 
-
-template <typename R, size_t N, typename CompList, typename Res, typename... Args>
-struct sys_function_traits<Res, N, CompList, R(*)(Args...)>
-{
-    using arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
-    using full_arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
-    static constexpr bool UsesTuple = false;
-    static constexpr bool UsesEntityID = false;
-    static constexpr bool UsesRes = false;
-};
-
-
-/*********************************************************************************************************
-
-
-
-
-
-
-
-
-**********************************************************************************************************/
+    template<typename R, size_t N, typename CompList, typename Res, typename... Args>
+    struct sys_function_traits<Res, N, CompList, R(*)(Args...)> {
+        using arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
+        using full_arg_types = FuncArgs<std::decay_t<std::remove_pointer_t<std::remove_cv_t<Args>>>...>;
+        static constexpr bool UsesTuple = false;
+        static constexpr bool UsesEntityID = false;
+        static constexpr bool UsesRes = false;
+    };
+}
